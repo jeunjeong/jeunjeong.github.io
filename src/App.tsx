63d28b6937324fc015f66c1980e58c6028631da8
조은정 <ejjeongs@naver.com>
2023-11-24 12:13:26 +0900

    ✨ feat : Add main routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Header from "./TopBar/top_bar";
 import Home from "./Home/Home";
 function App() {
 	return (
 		<div>
 			<Routes>
-				<Route path="/" element={<Home />} />
+				<Route path="/" element={[<Header />, <Home />]} />
 			</Routes>
 		</div>
 	);
