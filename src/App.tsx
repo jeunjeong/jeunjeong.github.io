7cd9de02bb44d79c9ff25b8bbd643e57a1436b41
조은정 <ejjeongs@naver.com>
2023-11-24 12:08:06 +0900

    � package : Add icon package / styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from "react";
 import "./App.css";
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home/Home";
 function App() {
-	return <div>HI</div>;
+	return (
+		<div>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Home/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Home.css";
+function Home() {
+	return <div>여기는 메인페이지</div>;
+}
+
+export default Home;
