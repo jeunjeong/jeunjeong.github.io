4d231165234325865052d629d48843efc94409df
조은정 <ejjeongs@naver.com>
2023-11-24 16:14:06 +0900

    ✨feat : Add about me page UI

---
+++ src/About Me/about_me.styled.tsx
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+	background-color: #bbbfca;
+	align-items: center;
+	padding-top: 2rem;
+	height: 700px;
+	text-align: center;
+`;
+
+export const Title = styled.div`
+	font-size: 5rem;
+	margin-bottom: 3rem;
+`;
+
+export const Content = styled.div`
+	display: flex;
+	font-size: 1.5rem;
+	margin-top: 1rem;
+	margin-bottom: 1rem;
+`;
+
+export const Section1 = styled.div`
+	width: 500px;
+	margin-left: 8rem;
+`;
+
+export const Section2 = styled.div`
+	margin-reft: 1rem;
+	margin-right: 1rem;
+
+	width: 500px;
+`;
+export const Section3 = styled.div`
+	margin-reft: 1rem;
+	margin-right: 1rem;
+	width: 500px;
+`;
+export const ProfileImg = styled.img`
+	height: 18rem;
+`;
+
+export const ProfileInfoDiv = styled.div`
+	margin-left: 100px;
+	text-align: left;
+`;
+export const ProfileInfo = styled.div`
+	margin-top: 0.6rem;
+	font-size: 1.3rem;
+`;
+
+export const ProfileTitle = styled.div`
+	margin-top: 1rem;
+	padding-left: 1rem;
+	text-align: left;
+	font-size: 1.6rem;
+	border-bottom: 1px solid black;
+`;
+
+export const ProfileContent = styled.div`
+	display: flex;
+	padding-top: 0.5rem;
+	padding-bottom: 0.5rem;
+	padding-left: 1rem;
+	text-align: left;
+	font-size: 1.3rem;
+`;
---
+++ src/About Me/progress_bar.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
+
+const ProgressWrap = styled.div`
+	background: pink;
+	margin: 5px 15px;
+	overflow: hidden;
+	position: relative;
+	width: 70%;
+	height: 20px;
+	border-radius: 10px;
+`;
+
+const ProgressBar = styled.div`
+	background-color: navy;
+	left: 0;
+	position: absolute;
+	top: 0;
+	height: 100%;
+	transition: width 1s ease-in-out;
+`;
+
+interface ProgressProps {
+	progressPercent: number;
+}
+
+const Progress: React.FC<ProgressProps> = ({ progressPercent }) => {
+	const [barWidth, setBarWidth] = React.useState(0);
+	const progressWrapRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (progressWrapRef.current) {
+			const getPercent = progressPercent / 100;
+			const progressTotal = getPercent * progressWrapRef.current.offsetWidth;
+			setBarWidth(progressTotal);
+		}
+	}, [progressPercent]);
+
+	return (
+		<ProgressWrap ref={progressWrapRef}>
+			<ProgressBar style={{ width: `${barWidth}px` }} />
+		</ProgressWrap>
+	);
+};
+
+export default Progress;
