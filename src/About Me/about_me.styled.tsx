6d65a4829a37f7740179bb21c750fd19e34b5d2c
조은정 <ejjeongs@naver.com>
2023-11-26 01:17:51 +0900

    :zap: feat : Modify about me css

--- src/About Me/about_me.styled.tsx
+++ src/About Me/about_me.styled.tsx
@@ -1,56 +1,43 @@
 import styled from "styled-components";
 
 export const MainContainer = styled.div`
-	background-color: #bbbfca;
+	background-color: #f4f4f2;
 	padding-top: 1rem;
 	text-align: center;
+	border-bottom : 1px solid lightgray;
 `;
 
 export const Title = styled.div`
-	font-size: 5rem;
+	font-size: 3rem;
+	font-weight : bold;
 	margin-bottom: 3rem;
 `;
 
 export const Content = styled.div`
 	font-size: 1.5rem;
 	margin-top: 1rem;
-	margin-bottom: 1rem;
-
+	margin-bottom:2em;
+	
 	@media (min-width: 768px) {
 		display: flex;
 	}
 `;
 
-export const Section1 = styled.div`
-	width: 100%;
-	margin-left: 3%;
-	margin-right: 3%;
-	margin-bottom: 1rem;
+export const Section = styled.div`
+	
+	flex: 1;
+	margin-left:1rem;
+	margin-right:1rem;
+	margin-bottom: 2rem;
 `;
 
-export const Section2 = styled.div`
-	margin-left: 3%;
-	margin-right: 3%;
-	margin-bottom: 1rem;
-
-	width: 100%;
-`;
-export const Section3 = styled.div`
-	margin-left: 3%;
-	margin-right: 3%;
-	margin-bottom: 1rem;
-	width: 100%;
-`;
 export const ProfileImg = styled.img`
 	height: 18rem;
 `;
 
-export const ProfileInfoDiv = styled.div`
-	text-align: left;
-`;
 export const ProfileInfo = styled.div`
 	margin-top: 0.6rem;
-	font-size: 1.2rem;
+	font-size: 1.1rem;
 
 	@media (min-width: 1200px) {
 		margin-left: 100px;
@@ -60,7 +47,6 @@ export const ProfileInfo = styled.div`
 `;
 
 export const ProfileTitle = styled.div`
-	margin-top: 1rem;
 	padding-left: 1rem;
 	text-align: left;
 	font-size: 1.6rem;
@@ -74,6 +60,7 @@ export const ProfileContent = styled.div`
 	padding-left: 1rem;
 	text-align: left;
 	font-size: 1rem;
+
 
 	@media (min-width: 1200px) {
 		display: flex;
