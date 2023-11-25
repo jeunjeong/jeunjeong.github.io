bf4fe22a71602599e0209a6b0f75c204e94a9f95
조은정 <ejjeongs@naver.com>
2023-11-26 01:19:22 +0900

    ✨feat : Add Skills page

--- src/Skills/skills.styled.tsx
+++ src/Skills/skills.styled.tsx
@@ -1,1 +1,60 @@
 import styled from "styled-components";
+
+export const MainContainer = styled.div`
+    background-color: #f4f4f2;
+	padding-top: 1rem;
+	text-align: center;
+    order-bottom : 1px solid lightgray;
+    `;
+
+export const Title = styled.div`
+	font-size: 3rem;
+	font-weight : bold;
+	margin-bottom: 3rem;
+`;
+
+export const Content = styled.div`
+	font-size: 1.5rem;
+	margin-top: 1rem;
+	margin-bottom:2em;
+	
+	@media (min-width: 768px) {
+		display: flex;
+	}
+`;
+
+export const Section = styled.div`
+	flex: 1;
+	margin-left:1rem;
+	margin-right:1rem;
+	margin-bottom: 1.5rem;    
+`;
+
+export const Card = styled.div`
+    background-color : white;
+    border-radius : 10px;
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
+    margin-bottom : 1.3rem;
+    text-align:center;
+
+`
+
+export const MiddleTitle = styled.div`
+    padding-top:0.5rem;
+    padding-bottom: 0.5rem;
+    font-size:1.5rem;
+`
+export const Item = styled.div`
+    display:flex;
+    justify-content: center;
+    padding : 0.5rem;
+`
+export const Icons = styled.img`
+    height: 1.5rem;
+    padding-right : 0.5rem;
+`
+
+export const IconsName = styled.div`
+    font-size:1.2rem;
+`
+
--- src/Skills/skills.tsx
+++ src/Skills/skills.tsx
@@ -1,11 +1,144 @@
 import React from "react";
-import { FaSquarePhone } from "react-icons/fa6";
-import { ImMail } from "react-icons/im";
-import { FaInstagramSquare } from "react-icons/fa";
-import { FaGithubSquare } from "react-icons/fa";
+import * as skills from "./skills.styled";
 
-function Home() {
-	return <div></div>;
+function Skills() {
+	return <skills.MainContainer>
+		<skills.Title>Skills</skills.Title>
+		<skills.Content>
+			<skills.Section>
+				<skills.Card>
+					<skills.MiddleTitle> Language </skills.MiddleTitle>
+					
+					<skills.Item>
+						<skills.Icons src="/icons/language/python.png"></skills.Icons>
+						<skills.IconsName>Python</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/language/C.png"></skills.Icons>
+						<skills.IconsName>C</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/language/C++.png"></skills.Icons>
+						<skills.IconsName>C++</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/language/java.png"></skills.Icons>
+						<skills.IconsName>Java</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/language/javascript.png"></skills.Icons>
+						<skills.IconsName>javascript</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/language/typescript.png"></skills.Icons>
+						<skills.IconsName>typescript</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/language/dart.png"></skills.Icons>
+						<skills.IconsName>Dart</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/language/matlab.png"></skills.Icons>
+						<skills.IconsName>matlab</skills.IconsName>
+					</skills.Item>
+				</skills.Card>
+				
+			</skills.Section>
+			<skills.Section>
+				<skills.Card>
+					<skills.MiddleTitle> Frontend </skills.MiddleTitle>
+					<skills.Item>
+						<skills.Icons src="/icons/frontend/HTML&CSS.png"></skills.Icons>
+						<skills.IconsName>HTML&CSS</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/frontend/Vue.png"></skills.Icons>
+						<skills.IconsName>Vue</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/frontend/react.png"></skills.Icons>
+						<skills.IconsName>React</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/frontend/Next.png"></skills.Icons>
+						<skills.IconsName>Next</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/frontend/flutter.png"></skills.Icons>
+						<skills.IconsName>flutter</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/frontend/recoil.png"></skills.Icons>
+						<skills.IconsName>recoil</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/frontend/axios.png"></skills.Icons>
+						<skills.IconsName>axios</skills.IconsName>
+					</skills.Item>
+				</skills.Card>
+				<skills.Card>
+					<skills.MiddleTitle> Embedded </skills.MiddleTitle>
+
+					<skills.Item>
+						<skills.Icons src="/icons/embedded/ros.png"></skills.Icons>
+						<skills.IconsName>ROS</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/embedded/vhdl.png"></skills.Icons>
+						<skills.IconsName>VHDL</skills.IconsName>
+					</skills.Item>
+				</skills.Card>
+			</skills.Section>
+			<skills.Section>
+				<skills.Card>
+					<skills.MiddleTitle> Backend </skills.MiddleTitle>
+					<skills.Item>
+						<skills.Icons src="/icons/backend/spring.png"></skills.Icons>
+						<skills.IconsName>Spring</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/backend/springboot.png"></skills.Icons>
+						<skills.IconsName>Spring Boot</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/backend/mysql.png"></skills.Icons>
+						<skills.IconsName>My SQL</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/backend/ec2.png"></skills.Icons>
+						<skills.IconsName>Amazon EC2</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/backend/postman.png"></skills.Icons>
+						<skills.IconsName>Postman</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/backend/swagger.png"></skills.Icons>
+						<skills.IconsName>Swagger</skills.IconsName>
+					</skills.Item>
+				</skills.Card>
+				<skills.Card>
+					<skills.MiddleTitle> Communication </skills.MiddleTitle>
+					<skills.Item>
+						<skills.Icons src="/icons/communication/git.png"></skills.Icons>
+						<skills.IconsName>Git</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/communication/gitlab.png"></skills.Icons>
+						<skills.IconsName>GitLab</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/communication/gerrit.png"></skills.Icons>
+						<skills.IconsName>Gerrit</skills.IconsName>
+					</skills.Item>
+					<skills.Item>
+						<skills.Icons src="/icons/communication/notion.png"></skills.Icons>
+						<skills.IconsName>notion</skills.IconsName>
+					</skills.Item>
+				</skills.Card>
+			</skills.Section>
+		</skills.Content>
+	</skills.MainContainer>;
 }
 
-export default Home;
+export default Skills;
