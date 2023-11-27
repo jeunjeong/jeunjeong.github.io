e8c7eab27be6886c532dcf2d49c2631897f9285d
조은정 <ejjeongs@naver.com>
2023-11-27 13:09:36 +0900

    :sparkles: feat : Modify carousel Add data

--- src/Project/card.tsx
+++ src/Project/card.tsx
@@ -1,16 +1,70 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
+import {
+	Atag,
+	CardContainer,
+	CardTitle,
+	CardSection,
+	CardImg,
+	CardInner,
+	CardMiddleTitle,
+	CardInnerUl,
+	CardInnerText,
+	ButtonDiv,
+	CardButton,
+	CardButtonDiv,
+} from "./card.style";
+import { IoIosArrowDroprightCircle } from "react-icons/io";
 
 interface CardProps {
-  title: string;
-  content: string;
-  img: string;
+	title: string;
+	contents: string[];
+	img: string;
+	rolls: string[];
+	github: string;
+	review: string[][];
 }
 
-const Card: FC<CardProps> = ({ title, content, img }) => (
-  <div className='card'>
-    <h2>{title}</h2>
-    <p><img src = {img}/> <div>{content}</div></p>
-  </div>
+const Card: FC<CardProps> = ({
+	title,
+	contents,
+	img,
+	rolls,
+	github,
+	review,
+}) => (
+	<CardContainer>
+		<CardTitle>{title}</CardTitle>
+		<CardSection>
+			<CardImg src={img} />
+			<CardInner>
+				<CardMiddleTitle>주제 소개</CardMiddleTitle>
+				{contents.map((content, i) => (
+					<CardInnerUl>
+						<CardInnerText>{content}</CardInnerText>
+					</CardInnerUl>
+				))}
+				<CardMiddleTitle>맡은 역할</CardMiddleTitle>
+				{rolls.map((roll, i) => (
+					<CardInnerUl>
+						<CardInnerText>{roll}</CardInnerText>
+					</CardInnerUl>
+				))}
+				<ButtonDiv>
+					<Atag href={github}>
+						<CardButton>
+							<CardButtonDiv>Go to Github&nbsp;</CardButtonDiv>
+							<IoIosArrowDroprightCircle />
+						</CardButton>
+					</Atag>
+
+					<CardButton>
+						<CardButtonDiv>Go to Review &nbsp;</CardButtonDiv>
+						<IoIosArrowDroprightCircle />
+					</CardButton>
+				</ButtonDiv>
+			</CardInner>
+		</CardSection>
+	</CardContainer>
 );
 
 export default Card;
